431ba8cc0255c06dd1e892c9e207742fd6948803
Евгений <@192.168.1.199>
2013-03-05 09:27:52 +0700

    head

---
+++ client/model/Timeline.js.js
@@ -0,0 +1,86 @@
+// позже разместим код модели
+// сейчас тут эксперементальные данные с которыми можно реботать
+// бла бла бла
+app.Timeline=function(){
+    return [
+	{
+            //демонстрационный первый обьект
+            title:"shape 1",
+	    target:{},
+	    prop:{
+                x:{
+                    0:{
+                        value:0,
+                        easing:"none"					
+                    },
+                    3:{
+                        value:100,
+                        easing:"line"										
+                    },				
+
+
+                    6:{
+                        value:100,
+                        easing:"none"					
+                    },
+                    10:{
+                        value:50,
+                        easing:"line"										
+                    }				
+                },
+
+                y:{
+                    3:{
+                        value:0,
+                        easing:"none"										
+                    },				
+
+                    8:{
+                        value:150,
+                        easing:"sin"					
+                    }
+                }
+            }
+
+        },
+        
+        //демонстрационный второй обьект
+        {
+            title:"shape 2",
+            target:{},
+            prop:{
+
+                x:{
+                    2:{
+                        value:100,
+                        easing:"none"					
+                    },
+                    3:{
+                        value:0,
+                        easing:"line"										
+                    },				
+
+                    6:{
+                        value:20,
+                        easing:"line"										
+                    }		
+                },
+
+                y:{
+                    0:{
+                        value:0,
+                        easing:"none"										
+                    },				
+
+
+                    8:{
+                        value:50,
+                        easing:"sin"					
+                    }
+
+                }
+            }
+
+        }	    	    
+    ]
+};

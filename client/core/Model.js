c5098b9900e569d2255d4c1dff95298477f58939
hnoe <hnoe@hnoe.ru>
2013-03-05 20:52:22 +0400

    Model.js

---
+++ client/core/Model.js
@@ -0,0 +1,13 @@
+// ========================================================================
+// Copyright 2012 hnoe
+// Email: hnoe@hnoe.ru
+// Site: http://hnoe.ru 
+//
+// Licensed under the GNU GPLv2
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+// http://www.gnu.org/licenses/gpl2.txt
+// ========================================================================
+
+тут будет Model.js

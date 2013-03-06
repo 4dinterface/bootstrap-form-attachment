66aa0e678d3051bec57355d4616b3caa7a4b2846
nerv <nerv-net@yandex.ru>
2013-03-06 20:03:25 +0300

    Добавлены стили Timeline.css

--- client/view/TimeLine.js
+++ client/view/TimeLine.js
@@ -1,36 +1,41 @@
-Define("app.view.Timeline", /** @lends {app.component} */{
-    extend:app.Component,
-    model:null,    
-    init: function (prop) {		
+Define( "app.view.Timeline", /** @lends {app.component} */{
+
+    extend: app.Component,
+
+    model: null,
+
+    init: function( prop ) {
         this.super();
-        this.apply(prop);
+        this.apply( prop );
         //this.model=prop;
-               
+
         //потом будет так
         //model.on('change',function(){
         //    this.render;            
         //});
-        
+
+        var a = app.model.Timeline();
+
         //здесь твой код
         this.render();
     },
-    
-    render:function(){
+
+    render: function() {
         //Это только демонстрационный код, от которого можно отталкнуться
-        
-        var m=this.model;
-        
+
+        var m = this.model;
+
         //цикл по обьектам
-        for (var i=0;i<m.length;i++){            
-            document.write(m[i].get('title')+"<br>");   
+        for( var i = 0; i < m.length; i++ ) {
+            document.write(m[i].get('title')+"<br>");
 
             //цикл по свойствам
-            for(var pi in m[i].get('prop')  ){
-               document.write(pi+"<br>");                                  
-            }            
-            
-        }	
+            for( var pi in m[i].get( 'prop' ) ) {
+                document.write(pi+"<br>");
+            }
 
-    }    
-    
-});
+        }
+
+    }
+
+} );

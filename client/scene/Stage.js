02e45940d5648740e0a00714fb27ea72e1d0e982
Евгений <@192.168.1.199>
2013-03-07 04:05:59 +0700

    based class for application 6 marth

---
+++ client/scene/Stage.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+app.scene={};
+app.scene.Stage=function(){
+  this.initialize(canvas);        
+  
+  
+}
+
+app.scene.Stage.prototype=createjs.Stage.prototype;

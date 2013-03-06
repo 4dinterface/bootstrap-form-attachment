c2bd82fd681b95d4ce7e38b17d3fb7788ec32b2c
nerv <nerv-net@yandex.ru>
2013-03-06 22:05:08 +0300

    Немного покодил, добавил комменты

--- client/view/TimeLine.js
+++ client/view/TimeLine.js
@@ -1,8 +1,14 @@
+/**
+ * Представление таймлана
+ *
+ * @returns {Object} Timeline объект представления таймлайна
+ */
+
 Define( "app.view.Timeline", /** @lends {app.component} */{
 
     extend: app.Component,
 
-    model: null,
+
 
     init: function( prop ) {
         this.super();
@@ -14,13 +20,25 @@ Define( "app.view.Timeline", /** @lends {app.component} */{
         //    this.render;            
         //});
 
-        var a = app.model.Timeline();
 
         //здесь твой код
+        // -----------------------
+
+        // установить отношение кол-ва пикслей к миллисекунде
+        this.options.set( 'ratio', 100 );
+
         this.render();
     },
 
+
+    /**
+    * Рисует/обновляет представление таймлана
+     *
+     * @this {Timeline}
+     */
     render: function() {
+
+        console.log( this.model );
         //Это только демонстрационный код, от которого можно отталкнуться
 
         var m = this.model;
@@ -36,6 +54,52 @@ Define( "app.view.Timeline", /** @lends {app.component} */{
 
         }
 
+    },
+
+
+    /**
+     *  Опции представления таймлайна. Прочитать/установить.
+     *
+     *  @this {Timeline}
+     *  @returns {Object} options
+     */
+    options: (function() {
+        var data = {};
+
+        return {
+            get: function( key ) {
+                return data[ key ];
+            },
+            set: function( key, value ) {
+                data[ key ] = value;
+            }
+        };
+    }()),
+
+
+    /**
+     *  Переводит миллисекунды в пиксили в зависимости
+     *  от настроек представления таймлана, например, зума.
+     *
+     *  @this {Timeline}
+     *  @param {Number} milliseconds
+     *  @returns {Number}
+     */
+    toPixels: function( milliseconds ) {
+        return milliseconds / 1000 * this.options.get( 'ratio' );
+    },
+
+
+    /**
+     *  Переводит пиксили в миллисекунды в зависимости
+     *  от настроек представления таймлана, например, зума.
+     *
+     *  @this {Timeline}
+     *  @param {Number} pixels
+     *  @returns {Number}
+     */
+    toMilliseconds: function( pixels ) {
+        return pixels / this.options.get( 'ratio' ) * 1000;
     }
 
 } );

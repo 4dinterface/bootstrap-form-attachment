3721da8a69b32dca45c42176ba0c87467528ea41
hnoe <hnoe@hnoe.ru>
2013-03-10 03:44:55 +0400

    драг энд дроп на канвасе

--- client/controller/Scene.js
+++ client/controller/Scene.js
@@ -1,8 +1,27 @@
 Define("app.controller.Scene", {
     extend: app.Component,
-    init: function (prop) {
-        this.super();
-        this.apply(prop);
-        alert("hello Hnoy");
-    }
+	init: function (prop) {
+		this.super();
+		//вставляет свойста в обьект
+		this.apply(prop);
+
+		// Drag-n-Drop на канвас
+		$(function () {
+			var elem = prop["stage"];
+
+			elem.addEventListener("mousedown", function (e) {
+				var o = elem.getObjectUnderPoint(e.stageX, e.stageY);
+				var offset = {
+					x : o.x - e.stageX,
+					y : o.y - e.stageY
+				};
+				e.addEventListener("mousemove", function (e) {
+					o.x = e.stageX + offset.x;
+					o.y = e.stageY + offset.y;
+					prop["stage"].update();
+				});
+			});
+		});
+	}
 });
+

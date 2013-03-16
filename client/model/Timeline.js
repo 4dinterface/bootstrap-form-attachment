a207f120b2aa0b0c389aec8bd888780578d1d261
diablo <4dinterface@mail.ru>
2013-03-16 14:16:50 +0700

    shape collection реализован как декоратор над массивом изменён тест buble для теста shapeCollection изменены тесты,  для запуска без сервера

--- client/model/Timeline.js
+++ client/model/Timeline.js
@@ -5,3 +5,65 @@ app.model.Timeline=function(){
     var data=[];
     return data;
 };
+
+Define('app.model.Timeline', /** @lends {app.Model} */ {
+    extend : app.Component,
+    data:null,
+    /***
+     * Конструктор экземпляров
+    * @constructor
+    * @param {Object} prop объект с описанием экземпляра
+    */
+    init : function () {
+        this.data=[];
+        this.super();                    
+    },  
+                
+    /**
+     * @method set
+     * @param {name} name
+     * @param {value} value
+     * @return null
+     **/
+    set : function (num, value) {        
+        this.data[i]=value;
+        this.fire("timelinechange", {
+            name:name,
+            value:value
+        });
+        
+        if(value.on) value.on('bubble',function(e){
+            me.fire(e.eventName,e);
+        })
+
+    },
+            
+    push : function (value) {        
+        var me=this;
+        this.data.push(value);
+        this.length=this.data.length;       
+        
+        this.fire("timelinechange", {
+            name:this.length,
+            value:value
+        });
+        
+        if(value.on) value.on('bubble',function(e){
+            me.fire(e.eventName,e);
+        })
+
+    },
+    
+    
+    /**
+     * @method get
+     * @param {property} name
+     * @param {value} value
+     * @return null
+     **/
+    get : function (num) {            
+        return this.data[num];        
+    }        
+    
+});
+

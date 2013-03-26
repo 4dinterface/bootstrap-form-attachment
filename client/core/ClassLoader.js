4902390cc1ad3be7e843750bd79f283a4bbadd12
diablo <4dinterface@mail.ru>
2013-03-26 21:37:24 +0700

    script loader

--- client/core/ClassLoader.js
+++ client/core/ClassLoader.js
@@ -1,8 +1,26 @@
 // компонент в разработке
-Define("app.classLoader",{       
-    //подгружает скрипты   
-    require:function(prop){
+Define("app.ClassLoader",{           
+    loadScript:function(src,callback){
+        var script = document.createElement("script");				
+	script.src = src+"?ns="+( Math.random()*1000 );
+	document.getElementsByTagName("head")[0].appendChild(script);
         
+	//обработчик загрузки
+	script.onload = function () {
+            callback();
+	};        
+    },
+    //подгружает скрипты   
+    require:function(prop,callback){
+        var me=this,
+            count=0;    
+        prop.forEach(function(el){
+            count++;
+            me.loadScript(el,function(){
+                count--;
+                if (count==0) callback();
+            });
+        })
     },
             
     creatorScript: function (src, parent) {

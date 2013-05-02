a16a78022fa512859d201622029f792d862c39ca
Евгений <@diablo2013.Dlink>
2013-05-03 03:57:02 +0700

    utilites fix

--- client/timeline/utilites.js
+++ client/timeline/utilites.js
@@ -1,21 +1,11 @@
 'use strict';
 
-Define( 'app.timeline.utilites', {
-
-    extend: app.Component,
-
-    /**
-     * Данные модели таймлайна
-     * @type {app.model.Timeline}
-     * @private
-     */
-    model: null,
-
-
-    init: function( cfg ) {
-        this._super();
-        this.apply( cfg );
-
+app.timeline=app.timeline||{};
+app.timeline.utilites={
+    exampleFunction1:function(){
+        
+    },
+    exampleFunction2:function(){
+        
     }
-
-});
+}

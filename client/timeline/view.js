6aa9c76cceb28f0c5504ccd61db5bc4706e6bdd4
nerv <nerv-net@yandex.ru>
2013-05-03 16:15:02 +0400

    Вырезаны утилиты

--- client/timeline/view.js
+++ client/timeline/view.js
@@ -18,13 +18,16 @@ Define( "app.timeline.view", /** @lends {app.component} */{
      */
     model: null,
 
+    utilites: app.timeline.utilites,
+
 
     init: function( cfg ) {
-        var me=this;
-        
         this._super();
         this.apply( cfg );
 
+
+        console.log( app.timeline.utilites );
+        console.log( this );
 
 
         // Предполагается, что событие срабатывает после готовности документа
@@ -52,24 +55,17 @@ Define( "app.timeline.view", /** @lends {app.component} */{
 
         // Перемещение бегунка при клике в области таймлайна
         this.model.on( 'oncursorchange', function( e ) {
-            this.movie.gotoAndStop( this.toMilliseconds( e.x ) );
+            this.movie.gotoAndStop( this.utilites.toMilliseconds( e.x ) );
         }.bind( this ));
 
 
         // Передвижение бегунка при воспроизведении анимации
         this.movie.on( 'onframe', function( e ) {
-            $( '#timeline-runner' ).css( 'left', this.toPixels( e.elapsedTime ) );
+            $( '#timeline-runner' ).css( 'left', this.utilites.toPixels( e.elapsedTime ) );
         }.bind( this ));
 
 
         // ------------------- END CURSOR ------------------------
-
-
-        // установить кол-во пикслей в секунде
-        this.options.set( 'ratio', 100 );
-
-        // установить зум
-        this.options.set( 'zoom', 1 );                
         
     },
 
@@ -96,7 +92,7 @@ Define( "app.timeline.view", /** @lends {app.component} */{
         var width = 800;
         var points = [];
 
-        var ms = this.toMilliseconds( 100 ) / this.options.get( 'zoom' );
+        var ms = this.utilites.toMilliseconds( 100 ) / this.model.zoom;
         var m;
 
         var x = [ 1000, 500, 250, 125, 100, 50, 25, 10, 5, 1 ];
@@ -109,7 +105,7 @@ Define( "app.timeline.view", /** @lends {app.component} */{
             }
         });
 
-        width = this.toPixels( m ) * this.options.get( 'zoom' );
+        width = this.utilites.toPixels( m ) * this.model.zoom;
 
         points = new Array( 800 / width | 0 );
 
@@ -122,9 +118,6 @@ Define( "app.timeline.view", /** @lends {app.component} */{
                 value: m * index
             };
         }
-
-        // console.log( m );
-        // console.log( this.toPixels( m ) * this.options.get( 'zoom' ) );
 
         $( '#timeline-ruler' ).jqotesub( '#template-timeline-ruler', points );
     },
@@ -223,7 +216,8 @@ Define( "app.timeline.view", /** @lends {app.component} */{
      * @return {Object} keyframes Массив ключей для создания разметки + сдвиг
      */
     createKeyframes: function( keyframesCollection, clazz ) {
-        var zoom = this.toPixels( this.options.get( 'zoom' ) ); // TODO: Не забыть про zoom
+
+        var zoom = this.utilites.toPixels( this.model.zoom ); // TODO: Не забыть про zoom
         var keyframes = [];
         var shift = 0;
 
@@ -264,52 +258,6 @@ Define( "app.timeline.view", /** @lends {app.component} */{
             shift: shift
         };
 
-    },
-
-
-    /**
-     *  Опции представления таймлайна. Прочитать/установить.
-     *
-     *  @this {Timeline}
-     *  @return {Object} options
-     */
-    options: (function() {
-        var data = {};
-
-        return {
-            get: function( key ) {
-                return data[ key ];
-            },
-            set: function( key, value ) {
-                data[ key ] = value;
-            }
-        };
-    }()),
-
-
-    /**
-     *  Переводит миллисекунды в пиксели в зависимости
-     *  от настроек представления таймлана.
-     *
-     *  @this {Timeline}
-     *  @param {Number} milliseconds
-     *  @return {Number}
-     */
-    toPixels: function( milliseconds ) {
-        return milliseconds / 1000 * this.options.get( 'ratio' );
-    },
-
-
-    /**
-     *  Переводит пиксели в миллисекунды в зависимости
-     *  от настроек представления таймлана.
-     *
-     *  @this {Timeline}
-     *  @param {Number} pixels
-     *  @return {Number}
-     */
-    toMilliseconds: function( pixels ) {
-        return pixels / this.options.get( 'ratio' ) * 1000;
     }
 
 });

138c6ff3749fb5d70bd16e04421b6688804d3af7
nerv <nerv-net@yandex.ru>
2013-05-03 16:12:47 +0400

    Добавлены утилиты для таймлайна

--- client/timeline/utilites.js
+++ client/timeline/utilites.js
@@ -1,11 +1,33 @@
 'use strict';
 
-app.timeline=app.timeline||{};
-app.timeline.utilites={
-    exampleFunction1:function(){
-        
+app.timeline = app.timeline || {};
+
+
+app.timeline.utilites = {
+
+    /**
+     *  Переводит миллисекунды в пиксели в зависимости
+     *  от настроек представления таймлана.
+     *
+     *  @this {Timeline}
+     *  @param {Number} milliseconds
+     *  @return {Number}
+     */
+    toPixels: function( milliseconds ) {
+        return milliseconds / 1000 * this.model.pixelsPerSecond;
     },
-    exampleFunction2:function(){
-        
+
+
+    /**
+     *  Переводит пиксели в миллисекунды в зависимости
+     *  от настроек представления таймлана.
+     *
+     *  @this {Timeline}
+     *  @param {Number} pixels
+     *  @return {Number}
+     */
+    toMilliseconds: function( pixels ) {
+        return pixels / this.model.pixelsPerSecond * 1000;
     }
-}
+
+};

e2352f2b6e6592e9e262c9d6c94af27b40c917aa
nerv <nerv-net@yandex.ru>
2014-01-04 23:47:59 +0300

    Updated: перемещение бегунка при клике

--- client/presentation/timelineEditor/timeline/panels/right/Controller.js
+++ client/presentation/timelineEditor/timeline/panels/right/Controller.js
@@ -16,16 +16,18 @@ Define( 'app.timeline.panels.Right.Controller', {
         this.dom.editor = this.utilites.getById('timeline-panel-right__editor');
         this.dom.editorBody = this.utilites.getById('timeline-panel-right__editor-body');
 
-        this.assign(this.dom.editorBody, this.handlers);
+        this.attach(this.dom.editorBody, this.events.editorBody);
     },
 
 
-    handlers: {
-        click: function(event) {
-            var offsetX = this.owner.view.getEditorOffsetX();
-            var x = event.pageX - offsetX;
-            var ms = this.utilites.toMilliseconds(this.owner.model.pixelsPerSecond, x);
-            this.owner.movie.gotoAndStop(ms);
+    events: {
+        editorBody: {
+            click: function(event) {
+                var offsetX = this.owner.view.getEditorOffsetX();
+                var x = event.pageX - offsetX;
+                var ms = this.utilites.toMilliseconds(this.owner.model.pixelsPerSecond, x);
+                this.owner.movie.gotoAndStop(ms);
+            }
         }
     }
 
